be86aa0e0af84ec652b7169ba51f65fbc6316b6b
dinadehbi <147596316+dinadehbi@users.noreply.github.com>
2024-03-29 04:02:43 +0000

    Add files via upload

---
+++ DinaFly/index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="fly.css">
+
+</head>
+<body>
+    <header>
+        <img src="logo.png" height="150px" width="150px";>
+        <div class="links">
+        <a href="#">Home</a>
+        <a href="#">Tours Page</a>
+        <a href="#">News</a>
+        <a href="#">Contact</a>
+        </div>
+    </header>
+    <div class="container">
+    <div class="div1">
+      <h1> Discover the best offers at this moment</h1>
+      </div>
+      </div>
+    <section class="section1">
+        <div class="ticket">
+            <img src="https://a.cdn-hotels.com/gdcs/production6/d781/3bae040b-2afb-4b11-9542-859eeb8ebaf1.jpg" height="200px" width="100%">
+            <h4>Istanbul</h4><br>
+
+            <div class="con">
+
+
+            <div class="con1">
+            <p>02 avr.</p>
+            <p>LYS
+            <img src="plane-solid.svg" height="20px" width="20px">
+            IST
+            </p>
+            </div>
+            <div class="con2">
+                <p>a partir de <h1 class="price">93$</h1></p>
+            </div>
+            </div>
+        </div>
+        <div class="ticket"></div>
+        <div class="ticket"></div>
+        <div class="ticket"></div>
+        <div class="ticket"></div>
+        <div class="ticket"></div>
+        <div class="ticket"></div>
+        <div class="ticket"></div>
+        <div class="ticket"></div>
+        
+    </section>
+</body>
+</html>
